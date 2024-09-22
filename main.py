dfea0ae888ef96bdd57f030a7c2d7cff2c937f16
Magomed993 <160238040+Magomed993@users.noreply.github.com>
2024-09-23 01:47:43 +0300

    Added the ability to enter data via excel

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from math import remainder
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 import datetime
+import pandas
+import collections
+from pprint import pprint
 
 env = Environment(
     loader=FileSystemLoader('.'),
@@ -27,9 +30,22 @@ def decline_years(n):
         else:
             return 'лет'
 
+excel_data_if = pandas.read_excel('wine.xlsx', sheet_name='Лист1', na_values=['N/A', 'NA'], keep_default_na=False)
+excel_data_if_dict = excel_data_if.to_dict(orient='records')
+
+excel_data_if_dict2 = pandas.read_excel('wine2.xlsx',
+                                        sheet_name='Лист1',
+                                        na_values=['N/A', 'NA'],
+                                        keep_default_na=False).to_dict(orient='records')
+wine_collection = collections.defaultdict(list)
+for index in excel_data_if_dict2:
+    wine_collection[index['Категория']].append(index)
+
 rendered_page = template.render(
     current_year=years_work,
     word_year=decline_years(years_work),
+    wines=wine_collection,
+    name_collections=wine_collection.keys()
 )
 
 with open('index.html', 'w', encoding="utf8") as file:
