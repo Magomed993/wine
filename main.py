179515ba599218d8b15b16c78bfe78580b212ee2
Magomed993 <160238040+Magomed993@users.noreply.github.com>
2024-09-23 12:58:38 +0300

    Corrected the code

--- main.py
+++ main.py
@@ -1,22 +1,10 @@
 from http.server import HTTPServer, SimpleHTTPRequestHandler
-from math import remainder
-
-from jinja2 import Environment, FileSystemLoader, select_autoescape
-import datetime
-import pandas
 import collections
-from pprint import pprint
+import datetime
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+import pandas
 
-env = Environment(
-    loader=FileSystemLoader('.'),
-    autoescape=select_autoescape(['html', 'xml'])
-)
 
-template = env.get_template('template.html')
-
-now_date = datetime.date.today()
-start_work = 1920
-years_work = now_date.year - start_work
 
 def decline_years(n):
     if 11 <= n % 100 <= 19:
@@ -30,26 +18,36 @@ def decline_years(n):
         else:
             return 'лет'
 
-excel_data_if = pandas.read_excel('wine.xlsx', sheet_name='Лист1', na_values=['N/A', 'NA'], keep_default_na=False)
-excel_data_if_dict = excel_data_if.to_dict(orient='records')
 
-excel_data_if_dict2 = pandas.read_excel('wine2.xlsx',
-                                        sheet_name='Лист1',
-                                        na_values=['N/A', 'NA'],
-                                        keep_default_na=False).to_dict(orient='records')
-wine_collection = collections.defaultdict(list)
-for index in excel_data_if_dict2:
-    wine_collection[index['Категория']].append(index)
+if __name__ == '__main__':
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html', 'xml'])
+    )
 
-rendered_page = template.render(
-    current_year=years_work,
-    word_year=decline_years(years_work),
-    wines=wine_collection,
-    name_collections=wine_collection.keys()
-)
+    template = env.get_template('template.html')
 
-with open('index.html', 'w', encoding="utf8") as file:
-    file.write(rendered_page)
+    now_date = datetime.date.today()
+    start_work = 1920
+    years_work = now_date.year - start_work
 
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-server.serve_forever()
+    excel_data_if_dict3 = pandas.read_excel('wine3.xlsx',
+                                            sheet_name='Лист1',
+                                            na_values=['N/A', 'NA'],
+                                            keep_default_na=False).to_dict(orient='records')
+    wine_collection = collections.defaultdict(list)
+    for index in excel_data_if_dict3:
+        wine_collection[index['Категория']].append(index)
+
+    rendered_page = template.render(
+        current_year=years_work,
+        word_year=decline_years(years_work),
+        wines=wine_collection,
+        name_collections=wine_collection.keys()
+    )
+
+    with open('index.html', 'w', encoding="utf8") as file:
+        file.write(rendered_page)
+
+    server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+    server.serve_forever()
